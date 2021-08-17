0a47d5964660cb939cd0afc761858c87ce602c2d
Oleg <obazylnikov@ggc.edu>
2021-08-17 15:32:56 -0400

    update /blog path to display all blog posts and brief info

--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -1,10 +1,44 @@
 import React from "react";
 import BlogLayout from "../../components/blogLayout";
-const Blogs = () => {
+import { GetStaticProps } from "next";
+import { getAllBlogPosts } from "../../utils/mdx";
+import Link from "next/link";
+import { Box, Heading, Text, Link as Lnk } from "@chakra-ui/react";
+
+type Props = {
+  posts: {
+    content: string;
+    data: {
+      title: string;
+      slug: string;
+      summary: string;
+      publishedAt: string;
+    };
+  }[];
+};
+const Blogs = ({ posts }: Props) => {
   return (
     <BlogLayout>
-      <>Looking Good!</>;
+      {posts.map((post, idx) => {
+        return (
+          <Box key={idx}>
+            <Link passHref={true} href={`/blog/${post.data.slug}`}>
+              <Lnk>
+                <Heading size="xl">{post.data.title}</Heading>
+              </Lnk>
+            </Link>
+            <Text>{post.data.summary}</Text>
+          </Box>
+        );
+      })}
     </BlogLayout>
   );
+};
+
+export const getStaticProps: GetStaticProps = async (context) => {
+  const posts = getAllBlogPosts();
+  return {
+    props: { posts },
+  };
 };
 export default Blogs;
--- utils/mdx.ts
+++ utils/mdx.ts
@@ -1,5 +1,6 @@
 import fs from "fs";
 import path from "path";
+import matter from "gray-matter";
 
 export function getFiles(type: string) {
   return fs.readdirSync(path.join(process.cwd(), "data", type), "utf-8");
@@ -10,4 +11,18 @@ export function getFileByslug(type: string, slug: string) {
     path.join(process.cwd(), "data", type, `${slug}.mdx`),
     "utf-8"
   );
+}
+
+export function getAllBlogPosts() {
+  const slugs = getFiles("blog");
+  const allPosts = slugs.map((slug) => {
+    slug = slug.replace(".mdx", "");
+    const { content, data } = matter(getFileByslug("blog", slug));
+    // console.log(data);
+    return {
+      content,
+      data: { slug, ...data },
+    };
+  });
+  return allPosts;
 }
