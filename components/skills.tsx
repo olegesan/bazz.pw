63a47357159f5eef5decaa1f5da8599771a4faf0
Oleg <obazylnikov@ggc.edu>
2021-09-28 17:17:38 -0400

    updates skills section

--- components/skills.tsx
+++ components/skills.tsx
@@ -4,22 +4,27 @@ import SubSection from "./subSection";
 import Description from "./description";
 
 export default function Skills() {
-  return (
-    <Section title="Skills">
-      <SubSection title="Frontend">
-        <Description>
-          React.JS · NextJS · Chakra-UI · HTML5 · CSS · JQuery · Bootstrap
-        </Description>
-      </SubSection>
-
-      <SubSection title="Backend">
-        <Description>
-          NodeJS · Django · Express.js · Flask · GraphQL · REST API · Laravel
-        </Description>
-      </SubSection>
-      <SubSection title="Programming">
-        <Description>JavaScript · Java · Python · PHP</Description>
-      </SubSection>
-    </Section>
-  );
+    return (
+        <Section title="Skills">
+            <SubSection title="Programming">
+                <Description>JavaScript · Python · Java · Go</Description>
+            </SubSection>
+            <SubSection title="Backend">
+                <Description>
+                    NodeJS · NestJS · Express · Flask · GraphQL · REST API ·
+                    MongoDB · SQL
+                </Description>
+            </SubSection>
+            <SubSection title="Frontend">
+                <Description>
+                    React · NextJS · TypeScript · Chakra-UI · HTML/CSS
+                </Description>
+            </SubSection>
+            <SubSection title="Development">
+                <Description>
+                    UML · Agile · Waterfall · OOP · VCS · Docker · AWS · Flutter
+                </Description>
+            </SubSection>
+        </Section>
+    );
 }
